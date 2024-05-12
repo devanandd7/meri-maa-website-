15d9bd641b4ac40733cb056270fb57fdef04d0ad
devanandd7 <128312249+devanandd7@users.noreply.github.com>
2024-05-12 10:41:10 +0530

    done analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,14 @@
 import "@/styles/globals.css";
 import Navbar from "./navbar";
+import { Analytics } from '@vercel/analytics/react';
+
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <Navbar /> 
       <Component {...pageProps} />
+      <Analytics />
     </>
   );
 }
