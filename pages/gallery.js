36d5d8cda8d390d9ac4e3ef25832802cae8b4592
devanandd7 <128312249+devanandd7@users.noreply.github.com>
2024-08-02 02:47:03 +0530

    before/after done

--- pages/gallery.js
+++ pages/gallery.js
@@ -8,24 +8,24 @@ function gallery() {
 <div class="grid grid-cols-2 md:grid-cols-4 gap-4">
     <div class="grid gap-4">
         <div>
-            <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image.jpg" alt=""/>
+            <img class="h-auto max-w-full rounded-lg" src="maa_light1.jpg" alt=""/>
         </div>
         <div>
-            <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-1.jpg" alt=""/>
+            <img class="h-auto max-w-full rounded-lg" src="onshirt.jpg" alt=""/>
         </div>
         <div>
-            <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-2.jpg" alt=""/>
+            <img class="h-auto max-w-full rounded-lg" src="meri-maa2.jpg" alt=""/>
         </div>
     </div>
     <div class="grid gap-4">
         <div>
-            <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-3.jpg" alt=""/>
+            <img class="h-auto max-w-full rounded-lg" src="meri-maa1.jpg" alt=""/>
         </div>
         <div>
-            <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-4.jpg" alt=""/>
+            <img class="h-auto max-w-full rounded-lg" src="allsketch.jpg" alt=""/>
         </div>
         <div>
-            <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-5.jpg" alt=""/>
+            <img class="h-auto max-w-full rounded-lg" src="maalight2.jpg" alt=""/>
         </div>
     </div>
     <div class="grid gap-4">
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,15 @@
 import { Inter } from "next/font/google";
+import { useEffect } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
-
-
 export default function Home() {
-  const Con=document.getElementById("codepen")
-  const slider = document.querySelector(".slider")
-  slider.addEventListener("input", (e) => {
-  Con.style.setProperty("--position", `${e.target.value}%`);
+  useEffect(() => {
+    const Con = document.getElementById("codepen");
+    const slider = document.querySelector(".slider");
+    slider.addEventListener("input", (e) => {
+      Con.style.setProperty("--position", `${e.target.value}%`);
+    });
   });
 
   return (
@@ -26,17 +27,19 @@ export default function Home() {
                 <h1 className="text-black-900 text-7xl title-font font-bold my-7 text-center">
                   Meri Maa
                 </h1>
-
+<h5 className="text-center">~ You think ,I made for You ~</h5>
                 <p className="leading-relaxed text-center mt-9">
-                  Welcome to our T-shirt selling website! We believe that family
-                  is at the heart of everything we do. Our mission is simple: to
-                  create meaningful connections through art.
+                  Welcome to our T-shirt <b>Design</b> website! Our mission is
+                  simple: to create meaningful connections through art.
                 </p>
+               
 
                 <h1 className="text-3xl  text-blue-600 text-center">
                   {" "}
                   Only 350₹{" "}
                 </h1>
+                <p className="text-center"><b>~❤~ <br />A gift to your memories and your loved One. <br /> ~❤~</b></p>
+               
               </div>
             </div>
           </div>
@@ -44,24 +47,119 @@ export default function Home() {
 
         <section className="text-gray-600 body-font">
           <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
-            <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-              <h1 className="title-font sm:text-5xl text-3xl mb-4 font-medium text-gray-900">
+          <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+              <div id="codepen" class="con">
+                <div class="image-container">
+                  <img
+                    class="image-before slider-image"
+                    src="meri-maa-org.jpg"
+                    alt="color photo"
+                  />
+                  <img
+                    class="image-after slider-image"
+                    src="meri-maa2.jpg"
+                    alt="black and white"
+                  />
+                </div>
+
+                <input
+                  type="range"
+                  min="0"
+                  max="100"
+                  value="50"
+                  aria-label="Percentage of before photo shown"
+                  class="slider"
+                />
+                <div class="slider-line" aria-hidden="true"></div>
+                <div class="slider-button" aria-hidden="true">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="30"
+                    height="30"
+                    fill="currentColor"
+                    viewBox="0 0 256 256"
+                  >
+                    <rect width="256" height="256" fill="none"></rect>
+                    <line
+                      x1="128"
+                      y1="40"
+                      x2="128"
+                      y2="216"
+                      fill="none"
+                      stroke="currentColor"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="16"
+                    ></line>
+                    <line
+                      x1="96"
+                      y1="128"
+                      x2="16"
+                      y2="128"
+                      fill="none"
+                      stroke="currentColor"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="16"
+                    ></line>
+                    <polyline
+                      points="48 160 16 128 48 96"
+                      fill="none"
+                      stroke="currentColor"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="16"
+                    ></polyline>
+                    <line
+                      x1="160"
+                      y1="128"
+                      x2="240"
+                      y2="128"
+                      fill="none"
+                      stroke="currentColor"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="16"
+                    ></line>
+                    <polyline
+                      points="208 96 240 128 208 160"
+                      fill="none"
+                      stroke="currentColor"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="16"
+                    ></polyline>
+                  </svg>
+                </div>
+              </div>
+
+              {/* <img
+                className="object-cover object-center rounded"
+                alt="hero"
+                src="meri-maa1.jpg"
+              /> */}
+            </div>
+            <div className="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
+              <h1 className="title-font sm:text-5xl text-3xl mb-4 font-medium text-yellow-200">
                 Costum Art-work
                 <br className=" lg:inline-block" />
-                <i className="sm:text-3xl text-yellow-200">
-                  (i can made for u)
+                <i className="sm:text-3xl text-gray-700">
+                  <p>Waterproof on any cloth</p>
                 </i>
               </h1>
-              <p className="mb-8 leading-relaxed text-white">
-                I weave graphite dreams onto paper ,cloth, saari ,jacket ,shirt.
-                My lines dance with emotion, capturing your essence—the curve of
-                your smile, the depth of your eyes. Let my sketches whisper your
-                story.
-              </p>
-              <div className="flex justify-center ">
+              <h6 className="mb-8 leading-relaxed text-white">
+                Sketch on  paper.
+                <br />
+                Sketch on  cloth/ acket/ shirt.
+               <br />
+                I guarantee that the sketch will not fade or the color will fade even after washing the clothes.
+                <br /> 
+                <p className="text-center"><b> <i><br />Stay Connected With Love One <br /></i> ~❤~</b></p>
+              </h6>
+              <div className="flex justify-item-center ">
                 <a href="https://forms.gle/mREbvifNpZu6GDQe6">
                   {" "}
-                  <button className="ml-4 inline-flex text-blue-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">
+                  <button className="ml-4 mx-auto inline-flex text-blue-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">
                     <i>
                       {" "}
                       <b> Order now</b>{" "}
@@ -70,102 +168,97 @@ export default function Home() {
                 </a>
               </div>
             </div>
-            <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
-          
-   
-                <div  id="codepen" class="con">
-                  <div class="image-container">
-                    <img
-                      class="image-before slider-image"
-                      src="meri-maa-org.jpg"
-                      alt="color photo"
-                    />
-                    <img
-                      class="image-after slider-image"
-                      src="meri-maa2.jpg"
-                      alt="black and white"
-                    />
-                  </div>
+           
+          </div>
+        </section>
 
-                  <input
-                    type="range"
-                    min="0"
-                    max="100"
-                    value="50"
-                    aria-label="Percentage of before photo shown"
-                    class="slider"
-                  />
-                  <div class="slider-line" aria-hidden="true"></div>
-                  <div class="slider-button" aria-hidden="true">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="30"
-                      height="30"
-                      fill="currentColor"
-                      viewBox="0 0 256 256"
-                    >
-                      <rect width="256" height="256" fill="none"></rect>
-                      <line
-                        x1="128"
-                        y1="40"
-                        x2="128"
-                        y2="216"
-                        fill="none"
-                        stroke="currentColor"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        stroke-width="16"
-                      ></line>
-                      <line
-                        x1="96"
-                        y1="128"
-                        x2="16"
-                        y2="128"
-                        fill="none"
-                        stroke="currentColor"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        stroke-width="16"
-                      ></line>
-                      <polyline
-                        points="48 160 16 128 48 96"
-                        fill="none"
-                        stroke="currentColor"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        stroke-width="16"
-                      ></polyline>
-                      <line
-                        x1="160"
-                        y1="128"
-                        x2="240"
-                        y2="128"
-                        fill="none"
-                        stroke="currentColor"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        stroke-width="16"
-                      ></line>
-                      <polyline
-                        points="208 96 240 128 208 160"
-                        fill="none"
-                        stroke="currentColor"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        stroke-width="16"
-                      ></polyline>
-                    </svg>
-                  </div>
-                </div>
-                
-      
-
-              {/* <img
-                className="object-cover object-center rounded"
-                alt="hero"
-                src="meri-maa1.jpg"
-              /> */}
+        <section class="text-gray-600 body-font">
+          <div class="container mx-2 box-border border-2 ">
+            <div class="text-center">
+              <h1 class="sm:text-3xl text-2xl font-medium text-center title-font text-gray-900">
+                What i do
+              </h1>
             </div>
+
+            <div class="p-2 sm:w-1/2 w-full">
+             
+            </div>
+            <div class="flex flex-wrap lg:w-4/5 sm:mx-auto sm:mb-2 -mx-2">
+              <div class="p-2 sm:w-1/2 w-full">
+                <div class="bg-gray-100 rounded flex p-4 h-full items-center">
+                  <svg
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="3"
+                    class="text-indigo-500 w-6 h-6 flex-shrink-0 mr-4"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M22 11.08V12a10 10 0 11-5.93-9.14"></path>
+                    <path d="M22 4L12 14.01l-3-3"></path>
+                  </svg>
+                  <span class="title-font font-medium">
+                  Waterproof drawing on cloth.
+                   
+                  </span>
+                </div>
+              </div>
+              <div class="p-2 sm:w-1/2 w-full ">
+                <div class="bg-gray-100 rounded flex p-4 h-full items-center ">
+                  <svg
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="3"
+                    class="text-indigo-500 w-6 h-6 flex-shrink-0 mr-4"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M22 11.08V12a10 10 0 11-5.93-9.14"></path>
+                    <path d="M22 4L12 14.01l-3-3"></path>
+                  </svg>
+                  <span class="title-font font-medium">Sketch Potrate</span>
+                </div>
+              </div>
+
+              <div class="p-2 sm:w-1/2 w-full">
+                <div class="bg-gray-100 rounded flex p-4 h-full items-center">
+                  <svg
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="3"
+                    class="text-indigo-500 w-6 h-6 flex-shrink-0 mr-4"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M22 11.08V12a10 10 0 11-5.93-9.14"></path>
+                    <path d="M22 4L12 14.01l-3-3"></path>
+                  </svg>
+                  <span class="title-font font-medium">Sketch Gift</span>
+                </div>
+              </div>
+              <div class="p-2 sm:w-1/2 w-full">
+                <div class="bg-gray-100 rounded flex p-4 h-full items-center">
+                  <svg
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="3"
+                    class="text-indigo-500 w-6 h-6 flex-shrink-0 mr-4"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M22 11.08V12a10 10 0 11-5.93-9.14"></path>
+                    <path d="M22 4L12 14.01l-3-3"></path>
+                  </svg>
+                  <span class="title-font font-medium">Sketch Gift</span>
+                </div>
+              </div>
+              
+            </div>
+           
           </div>
         </section>
 
@@ -198,6 +291,13 @@ export default function Home() {
                     <i>
                       {" "}
                       <b> Order now</b>{" "}
+                    </i>
+                  </button>
+                </a>
+                <a href="/gallery">
+                  <button className="ml-4 inline-flex text-orange-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">
+                    <i>
+                      <b> Gallery</b>
                     </i>
                   </button>
                 </a>
@@ -244,6 +344,3 @@ export default function Home() {
     </>
   );
 }
-
-
-
