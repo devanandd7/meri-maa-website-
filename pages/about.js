d4309c54e340c689f1fcec611334a58f5fd574c2
devanandd7 <128312249+devanandd7@users.noreply.github.com>
2024-08-11 16:15:08 +0530

    done

--- pages/about.js
+++ pages/about.js
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import React from "react";
 
 const about = () => {
@@ -62,7 +63,7 @@ const about = () => {
                   grandparents. Each image is carefully crafted to capture the
                   essence of the person, their love, and their unique story.
                 </p>
-<a href="https://forms.gle/mREbvifNpZu6GDQe6">
+<Link href="https://forms.gle/mREbvifNpZu6GDQe6">
                 <button className="flex items-center mt-auto text-white bg-orange-400 border-0 py-2 px-4 w-full focus:outline-none hover:bg-gray-500 rounded">
                   {" "}
                   Order now
@@ -77,7 +78,7 @@ const about = () => {
                   >
                     <path d="M5 12h14M12 5l7 7-7 7"></path>
                   </svg>
-                </button></a>
+                </button></Link>
               </div>
             </div>
 
@@ -104,7 +105,7 @@ const about = () => {
                   grandparents. Each image is carefully crafted to capture the
                   essence of the person, their love, and their unique story.
                 </p>
-                <a href="https://forms.gle/mREbvifNpZu6GDQe6">
+                <Link href="https://forms.gle/mREbvifNpZu6GDQe6">
 
                 <button className="flex items-center mt-auto text-white bg-orange-400 border-0 py-2 px-4 w-full focus:outline-none hover:bg-gray-500 rounded">
                   {" "}
@@ -120,7 +121,7 @@ const about = () => {
                   >
                     <path d="M5 12h14M12 5l7 7-7 7"></path>
                   </svg>
-                </button></a>
+                </button></Link>
               </div>
             </div>
 
@@ -147,7 +148,7 @@ const about = () => {
                   special, our T-shirts become keepsakes. They hold the
                   laughter, the tears, and the moments that define us.
                 </p>
-                <a href="https://forms.gle/mREbvifNpZu6GDQe6">
+                <Link href="https://forms.gle/mREbvifNpZu6GDQe6">
 
                 <button className="flex items-center mt-auto text-white bg-orange-400 border-0 py-2 px-4 w-full focus:outline-none hover:bg-gray-500 rounded">
                   {" "}
@@ -163,7 +164,7 @@ const about = () => {
                   >
                     <path d="M5 12h14M12 5l7 7-7 7"></path>
                   </svg>
-                </button></a>
+                </button></Link>
               </div>
             </div>
           </div>
