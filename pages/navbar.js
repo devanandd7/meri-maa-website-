bda8e3739076991dd10826399f5ddb4c0b1bdc7c
devanandd7 <128312249+devanandd7@users.noreply.github.com>
2024-08-11 16:59:20 +0530

    sunday 04:59

--- pages/navbar.js
+++ pages/navbar.js
@@ -6,7 +6,7 @@ const navbar = () => {
     <>
       <header className="text-white-600 bg-purple-900 ">
         <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-          <Link href="/" className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+          <Link href="/" className="flex title-font font-medium items-center  mb-4 md:mb-0">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
@@ -23,7 +23,7 @@ const navbar = () => {
               <b>Meri-Maa</b>
             </span>
           </Link>
-          <nav className="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
+          <nav className="md:ml-auto md:mr-auto flex flex-wrap items-center text-white justify-center">
             <Link className="mr-5 hover:text-cyan-200" href="/">
               Home
             </Link>
